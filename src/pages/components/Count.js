bcce1018e501c5f11ae3f5b717f4bcce7e732230
Renan Soares Souza <renansoares.oficiall@gmail.com>
2024-08-21 16:52:06 -0300

    Adicionando projeto ao GitHub

---
+++ src/pages/components/Count.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+function Count(){
+    const [count,setCount] = useState(0);
+    
+    function Incrementar(){
+            setCount(count +1);
+    }
+    function Decrementar(){
+        setCount(count -1);
+}
+    return (
+        
+        <div className="flex flex-col items-center ">
+            <label className="mb-16 text-4xl h-fit w-fit bg-gray-50 flex justify-center items-center p-2 rounded-md shadow-lg">{count}</label>
+            <div className='shadow-lg'>
+                <button className='bg-green-600 text-white font-bold p-2 rounded-tl-md  rounded-bl-md active:bg-green-900' onClick={Incrementar}>Incrementar</button>
+                <button className='bg-red-600 text-white font-bold p-2 rounded-tr-md  rounded-br-md active:bg-red-900' onClick={Decrementar}>Decrementar</button>
+            </div>
+        </div>
+    );
+}
+
+export default Count;
